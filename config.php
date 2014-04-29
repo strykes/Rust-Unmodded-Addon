d24963438db6366922b1f49f1f38872f5409f146
strykes <renebvip@hotmail.fr>
2014-04-30 00:47:27 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config["server_ip"] = 'SERVERIP';
+$config["server_queryport"] = 'SERVERQUERYPORT';
+$config["server_rcon"] = 'RCONPASSWORD';
+
+
+$admins = array();
+$admins["ADMINSTEAMCOMMUNITYID"] = "ADMINNAME";
+$admins["ADMINSTEAMCOMMUNITYID2"] = "ADMINNAME2";
+?>
