815986e3f3ce2b30bd5a3496694ac6bfee7b2c83
strykes <renebvip@hotmail.fr>
2014-04-30 22:18:12 +0200

    Create config.php

---
+++ settings/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config["server_ip"] = 'IP OF SERVER';
+$config["server_queryport"] = 'QUERY PORT OF SERVER';
+$config["server_rcon"] = 'RCON PASSWORD';
+$config["logs_folder"] = 'logs/';
+
+$admins = array();
+$admins["STEAMCOMMNUNITYID"] = "NAME";
+$admins["STEAMCOMMNUNITYID2"] = "NAME2";
+?>
