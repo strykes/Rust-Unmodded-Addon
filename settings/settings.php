68ff22e34219ba546e329f004374d86922432dcb
strykes <renebvip@hotmail.fr>
2014-04-30 22:19:17 +0200

    Create settings.php

---
+++ settings/settings.php
@@ -0,0 +1,57 @@
+#### Addon Settings ####
+
+## LOGGER
+## 0 = deactivated, 1 = activated
+LOGCONNECTIONS = 1
+LOGCHAT = 1
+
+
+
+## COMMANDS
+## 0 = Deactivated, 1 = All Users, 2 = Admins only
+who = 1
+day = 2
+night = 2
+time = 2
+kick = 2
+ban = 2
+tp = 2
+bring = 2
+scream = 2
+pvp = 2
+help = 1
+admintest = 1
+jail = 2
+slay = 2
+give = 2
+
+## MODULES
+## 0 = deactivated, 1 = activated
+broadcast_connections = 1
+restricted_names = 1
+restricted_dual_names = 1
+autowarnings = 1
+
+
+
+## JAIL SETTINGS
+## jail.x = X position
+## jail.y = Y position
+## jail.z = Z position
+## i strongly advise to use rustnuts.com to try and get your position, or connect on a server with /location enabled.
+jail.x = -6940
+jail.y = 360
+jail.z = -583
+free.x = 4786
+free.y = 598
+free.z = -4332
+
+## SLAY SETTINGS
+## slay.x = X position
+## slay.y = Y position
+## slay.z = Z position
+## i strongly advise to use rustnuts.com to try and get your position, or connect on a server with /location enabled.
+## You can either put a VERY high position and let the person die from fall (wont work with flyhackers ;)), or teleport into the sea, so you they instant die and you can go loot their stuff if they stole people.
+slay.x = 6215
+slay.y = 20
+slay.z = -5722
