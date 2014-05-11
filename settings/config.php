b9ed592c48533470394098ccfb4f38c0f902f4cc
strykes <renebvip@hotmail.fr>
2014-05-11 22:54:24 +0200

    Update config.php

--- settings/config.php
+++ settings/config.php
@@ -4,6 +4,11 @@ $config["server_ip"] = 'IP OF SERVER';
 $config["server_queryport"] = 'QUERY PORT OF SERVER';
 $config["server_rcon"] = 'RCON PASSWORD';
 $config["logs_folder"] = 'logs/';
+$config["mysql_host"] = "localhost";
+$config["mysql_login"] = "root";
+$config["mysql_pass"] = "";
+$config["mysql_database"] = "rust_db";
+$config["mysql_table"] = "rust";
 
 $admins = array();
 $admins["STEAMCOMMNUNITYID"] = "NAME";
