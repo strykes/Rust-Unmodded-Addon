d73d1edfcc090f91698c725e1a3576bea31fa7f3
strykes <renebvip@hotmail.fr>
2014-05-11 22:53:21 +0200

    Update main.php

--- main.php
+++ main.php
@@ -24,11 +24,18 @@ $playerlistarray = array();
 $spam = array();
 $data = pack("VV",1,03).$config["server_rcon"].chr(0).''.chr(0);
 $data = pack("V",strlen($data)).$data;
+$mysql = false;
+$dbb = false;
 fwrite($conn, $data, strlen($data));
-sendcmd("say \"[color #9999FF]Addon was successfully started\"");	
+//sendcmd("say \"[color #9999FF]Addon was successfully started\"");	
 
+if(GetVar("MYSQL") == 1)
+	$mysql = true;
 if(GetVar("automessages")==1)
 	$timers[] = array("time"=>time()+GetVar("automessages_timers"),"repeat_time"=>GetVar("automessages_timers"),"function"=>"sendautomessage","isarray"=>false,"repeat"=>true,"args"=>"");
+
+if($mysql)
+	$dbb = DBB($config["mysql_host"],$config["mysql_login"],$config["mysql_pass"],$config["mysql_database"],$config["mysql_table"]);
 
 while ($conn > 0) 
 {
@@ -56,12 +63,15 @@ while ($conn > 0)
 		  echo $receive."\n";
 	  $line = explode("\n",$receive);
 	  // CHAT LOGS
-	  if(GetVar("LOGCHAT")==1)
-		  if(strpos($receive,"[CHAT]") === 0)
-			  file_put_contents($config["logs_folder"]."/CHAT-".date("d-m-y",time()).".log",date("H:i:s",time()).":".substr($receive,7)."\n",FILE_APPEND);    
 	  // CHAT COMMAND HOOK
 	  if(preg_match('/\W[C][H][A][T]\W\W\W(.*?)\W\W\W(.*?)\W$/',$receive,$output))
 	  {
+		  if(GetVar("LOGCHAT")==1)
+		  {
+		 	 file_put_contents($config["logs_folder"]."/CHAT-".date("d-m-y",time()).".log",date("H:i:s",time()).":".substr($receive,7)."\n",FILE_APPEND);
+		 	 if($mysql)
+			 	put_in_mysql($dbb,"CHAT",$output[1],$output[2]);
+		  }
 		  {
 			  if(($lastmsg == time()) && !$checkmsg)
 			  {
@@ -84,24 +94,32 @@ while ($conn > 0)
 				  }
 			  }
 			  $lastmsg = time();
-		  }		  
-	  }
-	  if(preg_match('/\W[C][H][A][T]\W\W\W(.*?)\W\W\W\/(.*?)\W$/',$receive,$output))
-	  {
-		  chat_cmd($output[1],$output[2]);
+		  }		
+		  if(preg_match('/\W[C][H][A][T]\W\W\W(.*?)\W\W\W\/(.*?)\W$/',$receive,$output))
+		  {
+			  chat_cmd($output[1],$output[2]);
+		  }  
 	  }
 	  // CONNECTION HOOK
 	  elseif(preg_match('/^[A-Za-z]{4}\W[A-Za-z]{9}\W\W(.*?)\W\W([0-9]{17})\W$/',$receive,$output))
 	  {
 		  onuserconnect($output[1],$output[2]);
 		  if(GetVar("LOGCONNECTIONS")==1)
+		  {
 			  file_put_contents($config["logs_folder"]."/CONNECTIONS-".date("d-m-y",time()).".log",date("H:i:s",time()).":CONNECTED:".$output[1].":".$output[2]."\n",FILE_APPEND);   
+			  if($mysql)
+			 	put_in_mysql($dbb,"CONNECT",$output[1],$output[2]);  
+		  }
 	  }
 	  elseif(strpos($receive,"User Disconnected:")===0)
 	  {
 		onuserdisconnect(substr($receive,19)); 
 		if(GetVar("LOGCONNECTIONS")==1)
-			file_put_contents($config["logs_folder"]."/CONNECTIONS-".date("d-m-y",time()).".log",date("H:i:s",time()).":DISCONNECTED:".substr($receive,19)."\n",FILE_APPEND);   
+		{
+			file_put_contents($config["logs_folder"]."/CONNECTIONS-".date("d-m-y",time()).".log",date("H:i:s",time()).":DISCONNECTED:".substr($receive,19)."\n",FILE_APPEND);
+			if($mysql)
+			put_in_mysql($dbb,"DISCONNECT",substr($receive,19),"");  
+		}
 	  }
 	  elseif(isset($line) && isset($line[0]) && (strpos($line[0],"hostname:")===0))
 	  {
@@ -115,11 +133,16 @@ while ($conn > 0)
 	  elseif($receive == "OnDestroy")
 	  {
 		print("Server Shutdown");
+		put_in_mysql($dbb,"SHUTDOWN","SERVER SHUTDOWN","");  
 		break;  
 	  }
 	  elseif(preg_match("/^(.*?)\W[h][a][s]\W[s][u][i][c][i][d][e][d]$/",$receive,$output))
 	  {
 		  onusersuicide($output[1]);
+	  }
+	  elseif(($receive!="") && ($receive !="END") && ($mysql))
+	  {
+		  put_in_mysql($dbb,"CONSOLE","CONSOLE",$receive); 
 	  }
 	}
 	if(!$started)
